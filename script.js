7c6ab8ecafdc2b528f674826daa6578f498bbd0c
Andrew Moore <hello@andrewjamesmoore.com>
2022-01-02 12:30:42 -0700

    Refactored with functions
    Turned all of the query selectors into functions.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // console.log(document.querySelector('.message').textContent);
 
@@ -12,67 +12,75 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-//Variables
+//Variables and Functions
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-//Functions
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
-};
-
-//Highscore component
-
 let highScore = 0;
 
-//Again Handler
-document.querySelector('.again').addEventListener('click', function () {
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector(".number").textContent = number;
+};
+const displayScore = function (score) {
+  document.querySelector(".score").textContent = score;
+};
+const bodyColor = function (color) {
+  document.querySelector("body").style.backgroundColor = color;
+};
+const numberWidth = function (width) {
+  document.querySelector(".number").style.width = width;
+};
+const displayGuess = function (guess) {
+  document.querySelector(".guess").textContent = guess;
+};
+
+//Again Button Handler Function
+document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  displayMessage('Start Guessing...');
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.guess').value = '';
+  displayMessage("Start Guessing...");
+  displayNumber("?");
+  bodyColor("#222");
+  numberWidth("15rem");
+  displayScore(score);
+  displayGuess("");
 });
-//Button Handler Function
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+
+//Check Button Handler Function
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   //When there is no input
   if (!guess) {
-    // document.querySelector('.message').textContent = 'No Number!';
+    displayMessage("No Number!");
 
-    //Created function to replace the query selector for message
-    displayMessage('No Number!');
     //When Player Wins the game
   } else if (guess === secretNumber) {
-    displayMessage('Correct Number!');
+    displayMessage("Correct Number!");
+    displayNumber(secretNumber);
+    bodyColor("#60b347");
+    numberWidth("30rem");
 
-    document.querySelector('.number').textContent = secretNumber;
-
-    document.querySelector('body').style.backgroundColor = '#60b347';
-
-    document.querySelector('.number').style.width = '30rem';
-
+    //Record Score for highScore
     if (score > highScore) {
       highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
+      document.querySelector(".highscore").textContent = highScore;
     }
-
-    //Refactored High and Low Together
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
+      displayMessage(guess > secretNumber ? "Too High!" : "Too Low!");
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      displayMessage('You lost the game!');
-      document.querySelector('.score').textContent = 0;
+      displayMessage("You lost the game!");
+      displayScore(0);
     }
   }
+
+  //REFERENCE old code before refactoring
 
   //When guess is too high
   //   } else if (guess > secretNumber) {
